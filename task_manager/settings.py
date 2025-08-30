8e901a6f34e1cb54fd498646e5fe17784e0e15df
Vitaliy-Berezhnoy <vitaliyvitberezhnoy@gmail.com>
2025-08-30 18:39:51 +0300

    Fix settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -37,6 +37,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 ALLOWED_HOSTS = ['webserver',
                  '127.0.0.1',]
 
+# Automatic addition of a Render host
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
