9f6d47c0a3ca936ab9252e26e69e7798819657ee
Vitaliy-Berezhnoy <vitaliyvitberezhnoy@gmail.com>
2025-10-09 14:23:13 +0300

    Fix /users/forms.py

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,6 +1,6 @@
-from django.forms import CharField, PasswordInput, TextInput
+from django.forms import CharField, PasswordInput, TextInput, ModelForm
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from task_manager.users.models import User
 from django.utils.translation import gettext_lazy as _
 
@@ -48,7 +48,44 @@ class UserRegistrationForm(UserCreationForm):
             "Required field. No more than 150 characters. Only letters, numbers, and symbols @/./+/-/_."
         )}
 
-class UserUpdateForm(UserChangeForm):
+class UserUpdateForm(ModelForm):
+
+    password1 = CharField(
+        label=_("Password"),
+        widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': _("Password")}),
+        required=False,
+        help_text=_("Your password must contain at least 3 characters.")
+    )
+
+    password2 = CharField(
+        label=_("Password confirmation"),
+        widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': _("Password confirmation")}),
+        required=False,
+        help_text=_("Please enter the password again to confirm.")
+    )
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get("password1")
+        if password1 and len(password1) < 3:
+            raise ValidationError(_("Password must be at least 3 characters long"))
+        return password1
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 != password2:
+            raise ValidationError(_('The entered passwords do not match.'))
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get("password1")
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username']
@@ -57,19 +94,11 @@ class UserUpdateForm(UserChangeForm):
             'last_name': _('Surname'),
             'username': _('Username'),
         }
-
-    widgets = {
-        'first_name': TextInput(attrs={'class': 'form-control', 'placeholder': _('Name')}),
-        'last_name': TextInput(attrs={'class': 'form-control', 'placeholder': _('Surname')}),
-        'username': TextInput(attrs={'class': 'form-control', 'placeholder': _('Username')}),
-    }
-
-    help_text = {'username': _(
-        "Required field. No more than 150 characters. Only letters, numbers, and symbols @/./+/-/_."
-    )}
-
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Убираем поле пароля из формы редактирования
-        self.fields.pop('password', None)
+        widgets = {
+            'first_name': TextInput(attrs={'class': 'form-control', 'placeholder': _('Name')}),
+            'last_name': TextInput(attrs={'class': 'form-control', 'placeholder': _('Surname')}),
+            'username': TextInput(attrs={'class': 'form-control', 'placeholder': _('Username')}),
+        }
+        help_text = {'username': _(
+            "Required field. No more than 150 characters. Only letters, numbers, and symbols @/./+/-/_."
+        )}
